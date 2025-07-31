d10b13052345a7ee21a03da160ba18480a415e91
Tehelka <17590418+tehelka-gamedev@users.noreply.github.com>
2025-07-31 21:09:56 +0200

    Add alpha to room/npc colors

--- godot/scripts/Enum.gd
+++ godot/scripts/Enum.gd
@@ -1,3 +1,4 @@
+@tool
 extends Node
 # must be autoload Enum !
 
@@ -12,15 +13,15 @@ enum NPCColors {
 func color_enum_to_rgb(col: NPCColors) -> Color:
     match col:
         NPCColors.RED:
-            return Color.RED
+            return Color(1, 0, 0, 0.5)
         NPCColors.BLUE:
-            return Color.BLUE
+            return Color(0, 0, 1, 0.5)
         NPCColors.GREEN:
-            return Color.GREEN
+            return Color(0, 1, 0, 0.5)
         NPCColors.PINK:
-            return Color.PINK
+            return Color(0.8, 0.05, 0.8, 0.5)
         NPCColors.ORANGE:
-            return Color.ORANGE
+            return Color(0.8, 0.45, 0.05, 0.5)
 
         _:
             return Color.WHITE
