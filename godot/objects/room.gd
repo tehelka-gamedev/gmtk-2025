c8ed8a298c6d5d055a7fdc0a5c25e218d5dc7f0f
Tehelka <17590418+tehelka-gamedev@users.noreply.github.com>
2025-07-31 21:10:41 +0200

    Add color to rooms

--- godot/objects/room.gd
+++ godot/objects/room.gd
@@ -1,5 +1,12 @@
+@tool
 extends RoomBase
 class_name Room
+
+@export_category("Gameplay variables")
+@export var color: Enum.NPCColors = Enum.NPCColors.RED :
+    set(value):
+        color = value
+        modulate = Enum.color_enum_to_rgb(color)
 
 
 @onready var npc_spawn: Marker2D = $NPCSpawn
