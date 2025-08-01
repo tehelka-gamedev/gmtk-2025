bc6dad11f6471f9de1b2214716c660dd5fd6e3f3
Tehelka <17590418+tehelka-gamedev@users.noreply.github.com>
2025-08-01 19:42:21 +0200

    add pause menu (only with ESC for now)

--- godot/scripts/game_manager.gd
+++ godot/scripts/game_manager.gd
@@ -16,130 +16,134 @@ const NPC_class = preload("res://objects/npc/npc.tscn")
 var current_npc_count: int = 0
 
 func _ready() -> void:
-	randomize()
+    randomize()
 
-	if elevator == null:
-		push_error("no elevator")
-		return
-	
-	AudioManager.play_music(SoundBank.background_music)
-	# mute for now
-	AudioManager.mute_bgm(true)
+    if elevator == null:
+        push_error("no elevator")
+        return
+    
+    AudioManager.play_music(SoundBank.background_music)
+    # mute for now
+    AudioManager.mute_bgm(true)
 
-	for i in range(starting_npc_count):
-		_spawn_npc()
+    for i in range(starting_npc_count):
+        _spawn_npc()
 
-	elevator.door_opened.connect(_on_elevator_door_opened)
-	_restart_npc_spawn_timer()
-	
-	_control_panel.set_speed_cursor(elevator.current_speed)
-	_control_panel.speed_cursor_changed.connect(elevator.on_speed_value_changed)
-	_control_panel.open_gates_pressed.connect(elevator.on_open_gates)
-	_control_panel.close_gates_pressed.connect(elevator.on_close_gates)
-	_control_panel.start_elevator_pressed.connect(elevator.on_start_elevator)
-	_control_panel.stop_elevator_pressed.connect(elevator.on_stop_elevator)
+    elevator.door_opened.connect(_on_elevator_door_opened)
+    _restart_npc_spawn_timer()
+    
+    _control_panel.set_speed_cursor(elevator.current_speed)
+    _control_panel.speed_cursor_changed.connect(elevator.on_speed_value_changed)
+    _control_panel.open_gates_pressed.connect(elevator.on_open_gates)
+    _control_panel.close_gates_pressed.connect(elevator.on_close_gates)
+    _control_panel.start_elevator_pressed.connect(elevator.on_start_elevator)
+    _control_panel.stop_elevator_pressed.connect(elevator.on_stop_elevator)
 
 
 func _spawn_npc() -> void:
-	var non_full_rooms: Array[Room] = []
-	for room: Room in rooms:
-		if not room.slot_manager.is_full():
-			non_full_rooms.append(room)
+    var non_full_rooms: Array[Room] = []
+    for room: Room in rooms:
+        if not room.slot_manager.is_full():
+            non_full_rooms.append(room)
 
-	if len(non_full_rooms) == 0:
-		return
+    if len(non_full_rooms) == 0:
+        return
 
-	var random_room: Room = non_full_rooms.pick_random()
-	var spawn_position: Vector2 = random_room.npc_spawn.global_position
-	var npc: NPC = NPC_class.instantiate()
-	_npcs.add_child(npc)
-	npc.position = spawn_position
+    var random_room: Room = non_full_rooms.pick_random()
+    var spawn_position: Vector2 = random_room.npc_spawn.global_position
+    var npc: NPC = NPC_class.instantiate()
+    _npcs.add_child(npc)
+    npc.position = spawn_position
 
-	var available_colors: Array = Enum.NPCColors.values()
-	available_colors.erase(random_room.color)
-	npc.color = available_colors.pick_random()
+    var available_colors: Array = Enum.NPCColors.values()
+    available_colors.erase(random_room.color)
+    npc.color = available_colors.pick_random()
 
-	var slot: Slot = random_room.slot_manager.get_first_available_slot()
-	random_room.add_npc_inside(npc, slot)
-	npc.go_to_slot(slot)
+    var slot: Slot = random_room.slot_manager.get_first_available_slot()
+    random_room.add_npc_inside(npc, slot)
+    npc.go_to_slot(slot)
 
 func _on_elevator_door_opened() -> void:
-	var snapped_room: Room = elevator.get_snapped_room()
-	if snapped_room == null:
-		return # do nothing, if somehow the door opened without snapping
-	
-	# Start releasing people
-	var spawn_point:Marker2D = snapped_room.get_spawn_point()
-	var npc_to_release: NPC = null
+    var snapped_room: Room = elevator.get_snapped_room()
+    if snapped_room == null:
+        return # do nothing, if somehow the door opened without snapping
+    
+    # Start releasing people
+    var spawn_point:Marker2D = snapped_room.get_spawn_point()
+    var npc_to_release: NPC = null
 
-	# Find first npc with matching colour
-	var filter_to_room_color: Callable = func(npcs: Array[NPC]):
-		# iterate backward for better performance (negligible, but still)
-		for i in range(len(npcs)-1, -1, -1):
-			if npcs[i].color == snapped_room.color:
-				return npcs.pop_at(i)
-		return null
+    # Find first npc with matching colour
+    var filter_to_room_color: Callable = func(npcs: Array[NPC]):
+        # iterate backward for better performance (negligible, but still)
+        for i in range(len(npcs)-1, -1, -1):
+            if npcs[i].color == snapped_room.color:
+                return npcs.pop_at(i)
+        return null
 
-	elevator.start_loading_people()
-	while not elevator.is_empty():
-		# TODO: add a filter to only let people that want to leave out
-		npc_to_release = elevator.pop_npc_from_inside(filter_to_room_color)
-		
-		# no more matching the filter, stop
-		if npc_to_release == null:
-			break
+    elevator.start_loading_people()
+    while not elevator.is_empty():
+        # TODO: add a filter to only let people that want to leave out
+        npc_to_release = elevator.pop_npc_from_inside(filter_to_room_color)
+        
+        # no more matching the filter, stop
+        if npc_to_release == null:
+            break
 
-		var targets: Array[Node2D] = [
-			elevator.get_entrance_position(),
-			snapped_room.get_entrance_position(),
-			spawn_point,
-			]
-		npc_to_release.exiting = true
-		npc_to_release.state_machine.transition_to(
-			NPCStatesUtil.StatesName.move_to,
-			{
-				NPCStatesUtil.Message.target: targets
-			}
-		)
-		await npc_to_release.arrived_at_slot
+        var targets: Array[Node2D] = [
+            elevator.get_entrance_position(),
+            snapped_room.get_entrance_position(),
+            spawn_point,
+            ]
+        npc_to_release.exiting = true
+        npc_to_release.state_machine.transition_to(
+            NPCStatesUtil.StatesName.move_to,
+            {
+                NPCStatesUtil.Message.target: targets
+            }
+        )
+        await npc_to_release.arrived_at_slot
 
-	_on_all_npc_released()
+    _on_all_npc_released()
 
 func _on_all_npc_released() -> void:
-	var snapped_room: Room = elevator.get_snapped_room()
-	if snapped_room == null:
-		return # do nothing, if somehow the door opened without snapping
+    var snapped_room: Room = elevator.get_snapped_room()
+    if snapped_room == null:
+        return # do nothing, if somehow the door opened without snapping
 
-	# Start entering the elevator
-	var npc_to_enter: NPC = null
+    # Start entering the elevator
+    var npc_to_enter: NPC = null
 
-	while not elevator.is_full() and not snapped_room.is_empty():
-		npc_to_enter = snapped_room.pop_npc_from_inside()
+    while not elevator.is_full() and not snapped_room.is_empty():
+        npc_to_enter = snapped_room.pop_npc_from_inside()
 
-		# elevator is not full so there is at least one slot
-		var slot = elevator.slot_manager.get_first_available_slot() # not null :)
-		elevator.add_npc_inside(npc_to_enter, slot)
-		var targets: Array[Node2D] = [
-			snapped_room.get_entrance_position(),
-			elevator.get_entrance_position(),
-			slot,
-			]
-		npc_to_enter.state_machine.transition_to(
-			NPCStatesUtil.StatesName.move_to,
-			{
-				NPCStatesUtil.Message.target: targets
-			}
-		)
-		await npc_to_enter.arrived_at_slot
-		
-	elevator.stop_loading_people()
+        # elevator is not full so there is at least one slot
+        var slot = elevator.slot_manager.get_first_available_slot() # not null :)
+        elevator.add_npc_inside(npc_to_enter, slot)
+        var targets: Array[Node2D] = [
+            snapped_room.get_entrance_position(),
+            elevator.get_entrance_position(),
+            slot,
+            ]
+        npc_to_enter.state_machine.transition_to(
+            NPCStatesUtil.StatesName.move_to,
+            {
+                NPCStatesUtil.Message.target: targets
+            }
+        )
+        await npc_to_enter.arrived_at_slot
+        
+    elevator.stop_loading_people()
 
 
 func _on_npc_spawn_timer_timeout() -> void:
-	_spawn_npc()
-	_restart_npc_spawn_timer()
+    _spawn_npc()
+    _restart_npc_spawn_timer()
 
 
 func _restart_npc_spawn_timer() -> void:
-	var random_respawn_time: float = randf_range(MIN_NPC_RESPAWN_TIME, MAX_NPC_RESPAWN_TIME)
-	_npc_spawn_timer.start(random_respawn_time)
+    var random_respawn_time: float = randf_range(MIN_NPC_RESPAWN_TIME, MAX_NPC_RESPAWN_TIME)
+    _npc_spawn_timer.start(random_respawn_time)
+
+func _unhandled_input(event):
+    if event.is_action_pressed("ui_cancel"):
+        Events.pause()
