187c2a939c0609b3cbb5fa62b81f2e869cb694a4
Tehelka <17590418+tehelka-gamedev@users.noreply.github.com>
2025-08-01 16:15:05 +0200

    Access ControlPanel via unique name, parent it with CanvasLayer, and add MuteSoundContainer

--- godot/ui/mute_sounds_container.gd
+++ godot/ui/mute_sounds_container.gd
@@ -5,31 +5,40 @@ extends MarginContainer
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	if mute_se_button == null:
-		push_error("No mute sound effects button found in %s" % self)
-		return
-	
-	if mute_bgm_button == null:
-		push_error("No mute background music button found in %s" % self)
-		return
+    if mute_se_button == null:
+        push_error("No mute sound effects button found in %s" % self)
+        return
+    
+    if mute_bgm_button == null:
+        push_error("No mute background music button found in %s" % self)
+        return
 
-	mute_se_button.toggled.connect(_on_mute_se_button_toggled)
-	
-	mute_bgm_button.toggled.connect(_on_mute_bgm_button_toggled)
-	
-	
-	#if OS.has_feature("editor"):
-		#mute_bgm_button.set_pressed_no_signal(true)
-		#_on_mute_bgm_button_toggled(true)
-	
-	var is_muted:bool = AudioManager.is_bgm_muted()
-	mute_bgm_button.set_pressed_no_signal(is_muted)
-	_on_mute_bgm_button_toggled(is_muted)
+    mute_se_button.toggled.connect(_on_mute_se_button_toggled)
+    
+    mute_bgm_button.toggled.connect(_on_mute_bgm_button_toggled)
+    
+    
+    #if OS.has_feature("editor"):
+        #mute_bgm_button.set_pressed_no_signal(true)
+        #_on_mute_bgm_button_toggled(true)
+    
+    var is_muted:bool = AudioManager.is_bgm_muted()
+    mute_bgm_button.set_pressed_no_signal(is_muted)
+    _on_mute_bgm_button_toggled(is_muted)
+
+    AudioManager.BGM_mute_state_changed.connect(func(_is_muted: bool):
+        mute_bgm_button.set_pressed_no_signal(_is_muted)
+    )
+
+    AudioManager.SE_mute_state_changed.connect(func(_is_muted: bool):
+        mute_se_button.set_pressed_no_signal(_is_muted)
+    )
+
 
 
 
 func _on_mute_se_button_toggled(toggled_on:bool):
-	AudioManager.mute_se(toggled_on)
+    AudioManager.mute_se(toggled_on)
 
 func _on_mute_bgm_button_toggled(toggled_on:bool):
-	AudioManager.mute_bgm(toggled_on)
+    AudioManager.mute_bgm(toggled_on)
