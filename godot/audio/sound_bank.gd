2387c30f556d51d422eff18cc6e344eae31cdcff
Tehelka <17590418+tehelka-gamedev@users.noreply.github.com>
2025-08-01 14:06:33 +0200

    add bgm audio

--- godot/audio/sound_bank.gd
+++ godot/audio/sound_bank.gd
@@ -4,5 +4,6 @@ extends Node
 @export var main_menu_music: AudioStream = null
 @export var background_music: AudioStream = null
 @export var background_music_complicated: AudioStream = null
+@export var elevator_jingle_music: AudioStream = null
 
 @export_category("SE")
