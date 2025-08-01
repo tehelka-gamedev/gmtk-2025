e5da28fb30c728d87dcd73297e7d7b005ffe3f0b
Tehelka <17590418+tehelka-gamedev@users.noreply.github.com>
2025-08-01 13:15:19 +0200

    Add soundbank singleton

---
+++ godot/audio/sound_bank.gd
@@ -0,0 +1,8 @@
+extends Node
+
+@export_category("BGM")
+@export var main_menu_music: AudioStream = null
+@export var background_music: AudioStream = null
+@export var background_music_complicated: AudioStream = null
+
+@export_category("SE")
