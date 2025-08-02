d6776bcb13b1252dd5ce51a3430a703b718a7fa6
Gr0minet <36596087+Gr0minet@users.noreply.github.com>
2025-08-02 12:40:10 +0200

    Add decals

--- godot/objects/room.gd
+++ godot/objects/room.gd
@@ -2,18 +2,29 @@
 extends RoomBase
 class_name Room
 
+
 @export_category("Gameplay variables")
 @export var color: Enum.NPCColors = Enum.NPCColors.RED :
     set(value):
         color = value
-        modulate = Enum.color_enum_to_rgb(color)
+        _decal.modulate = Enum.color_enum_to_rgb(color)
 
 
 @export var entrance_position:Marker2D = null
+@export var decal_to_use: bool = false:
+    set(value):
+        if not ready:
+            await is_node_ready()
+        decal_to_use = value
+        if decal_to_use:
+            _decal.region_rect.position.x = 124
+        else:
+            _decal.region_rect.position.x = 0
 
 @onready var npc_spawn: Marker2D = $NPCSpawn
 @onready var _door: Sprite2D = $Door
 @onready var pivot: Node2D = $Pivot
+@onready var _decal: Sprite2D = $Pivot/Decal
 
 
 func get_spawn_point() -> Marker2D:
