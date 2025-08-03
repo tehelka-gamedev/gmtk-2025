ff376abd361242fc376192cd65d7269ac56846b3
Tehelka <17590418+tehelka-gamedev@users.noreply.github.com>
2025-08-03 12:42:47 +0200

    Improve control panel, add button with small light, remove mutesoundcontainer from main_menu

---
+++ godot/ui/control_panel/mute_audio_buttons/mute_bgm_button.gd
@@ -0,0 +1,37 @@
+extends MarginContainer
+
+@export var button:Button = null
+@export var small_light: SmallLight = null
+
+func _ready():
+    if button == null:
+        push_error("No button found in %s" % name)
+        return
+    
+    if small_light == null:
+        push_error("No small light found in %s" % name)
+        return
+
+    button.pressed.connect(_on_button_pressed)
+    
+    var is_muted = AudioManager.is_bgm_muted()
+    refresh_light(is_muted)
+
+    AudioManager.BGM_mute_state_changed.connect(
+        func(_is_muted: bool):
+            refresh_light(_is_muted)
+    )
+
+
+func _on_button_pressed() -> void:
+    # toggle SE
+    var new_mute_state: bool = not AudioManager.is_bgm_muted()
+    AudioManager.mute_bgm(new_mute_state)
+
+    refresh_light(new_mute_state)
+    
+func refresh_light(muted: bool) -> void:
+    small_light.set_template(
+        SmallLight.LightTemplate.NOT_OK if muted
+        else SmallLight.LightTemplate.OK
+    )
