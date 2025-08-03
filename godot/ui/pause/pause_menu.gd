55c5e9742ea738d4a26de19c6f1020014661d120
Tehelka <17590418+tehelka-gamedev@users.noreply.github.com>
2025-08-03 15:58:51 +0200

    add and use a pause_game action for pausing, and change display text placeholder

--- godot/ui/pause/pause_menu.gd
+++ godot/ui/pause/pause_menu.gd
@@ -12,7 +12,7 @@ func _unhandled_input(event: InputEvent) -> void:
     if not visible:
         return
     
-    if event.is_action_pressed("ui_cancel"):
+    if event.is_action_pressed("pause_game"):
         get_viewport().set_input_as_handled()
         Events.unpause()
     # elif event.is_action_pressed("exit"):
