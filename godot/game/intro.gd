d7c240ab3718aab235aa86380e3e3e877eeaa4ce
Tehelka <17590418+tehelka-gamedev@users.noreply.github.com>
2025-08-03 14:57:13 +0200

    Add background to intro (for corners) and lower music while dialog

--- godot/game/intro.gd
+++ godot/game/intro.gd
@@ -7,10 +7,12 @@ var va_sound: AudioStreamPlayer = null
 
 
 func _ready() -> void:
+    AudioManager.set_bgm_volume(20)
     va_sound = AudioManager.play_sound_effect(SoundBank.intro_bossletter)
 
 
 func _on_button_pressed() -> void:
     if va_sound:
         va_sound.stop()
+    AudioManager.set_bgm_volume(100)
     get_tree().change_scene_to_packed(game_scene)
